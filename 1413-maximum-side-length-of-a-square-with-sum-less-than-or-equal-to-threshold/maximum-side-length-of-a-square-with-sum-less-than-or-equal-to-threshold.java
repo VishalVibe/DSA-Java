3b678418d5a39c5aa2f3a9a020285cdbf3258a7d
Vishal Yadav <136216047+VishalVibe@users.noreply.github.com>
2026-01-19 12:26:45 +0530

    Time: 7 ms (82.74%) | Memory: 57.7 MB (58.38%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,33 +1,40 @@
 class Solution {
     public int maxSideLength(int[][] mat, int threshold) {
-        int m = mat.length ; 
-        int n = mat[0].length ; 
-        int maxSide = 0;
-        int[][] rowSum = new int[m][n+1];
-        int[][] colSum = new int[m+1][n];
+        int m = mat.length , n = mat[0].length ; 
+        int[][] prefix = new int[m+1][n+1];
 
-        for(int i=0 ; i<m ; i++){
-            for(int j = 0; j<n ; j++){
-                rowSum[i][j+1] = rowSum[i][j] + mat[i][j];
-                colSum[i+1][j] = colSum[i][j] + mat[i][j];
-
+        for(int i=0 ; i< m ; i++){
+            for(int j=0 ; j<n ; j++){
+                prefix[i+1][j+1] = prefix[i+1][j] + prefix[i][j+1] - prefix[i][j] + mat[i][j];
             }
         }
 
-        for(int k = Math.min(m , n) ; k>0 ; k--){
-            for(int r=0 ; r+k<=m ; r++){
-                for(int c = 0 ; c+k <= n ; c++){
-                    int sum = 0 ;
-                    for(int i=0 ; i< k ; i++){
-                        sum += rowSum[r+i][c+k] - rowSum[r+i][c];
+        int low = 0 , high = Math.min(m,n);
 
-                    }
-                    if(sum <= threshold){
-                        maxSide = Math.max(k , maxSide);
-                    }
+        while(low<high){
+            int mid = (high + low+1)/2;
+            if(existSquare(prefix , mid , threshold)){
+                low = mid;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+
+    private boolean existSquare(int[][] prefix , int k , int threshold){
+        int m = prefix.length-1 , n = prefix[0].length-1 ;
+
+        for(int i=0 ; i+k<= m ; i++){
+            for(int j=0 ; j+k<= n ; j++){
+                int sum = prefix[i+k][j+k] - prefix[i+k][j] - prefix[i][j+k] + prefix[i][j];
+                if(sum<= threshold){
+                    return true;
                 }
             }
         }
-        return maxSide;
+
+        return false;
     }
 }
